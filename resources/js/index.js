ebfd621c4dc4c47fe4cfbe4c9abf7c15d21c1b39
DonMadden <maddendw@mail.uc.edu>
2025-02-06 14:33:24 -0500

    I added in the refrences that are needed for the next part.

---
+++ resources/js/index.js
@@ -0,0 +1,4 @@
+const nameInput = document.getElementById("nameInput");
+const messageInput = document.getElementById("messageInput");
+const sendButton = document.getElementById("sendButton");
+const chatBox = document.getElementById("chatBox");

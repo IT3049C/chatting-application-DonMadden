c78cbec0596679b8c5a7a2c3ddef034d6222b65a
DonMadden <maddendw@mail.uc.edu>
2025-02-06 14:50:17 -0500

    firstly I added the format for message and I also added in the date and time then after that I did and if statement for my message vs your message.

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,3 +2,32 @@ const nameInput = document.getElementById("my-name-input");
 const messageInput = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
+
+function formatmessage(message, myNameInput){
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+}
+if (myNameInput === message.sender) {
+  return 
+  <div class="mine messages">
+    <div class="message">
+      ${message.text}
+    </div>
+    <div class="sender-info">
+      ${formattedTime}
+    </div>
+  </div>
+  ;
+}
+else {
+  return
+  <div class="yours messages">
+    <div class="message">
+      ${message.text}
+    </div>
+    <div class="sender-info">
+      ${message.sender} ${formattedTime}
+    </div>
+  </div>
+  ;
+}
